e28a3183738e085e030e10a6094ae7cf322ad24e
milan-mgu <mgu@narola.email>
2018-04-06 17:03:24 +0530

    Update ApiCrypter.php
    Replaced deprecated methods

--- FoodScanApp/ApiCrypter.php
+++ FoodScanApp/ApiCrypter.php
@@ -9,66 +9,18 @@
 
 class Security {
      public static function encrypt($input, $key) {
-        $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
-        $input = Security::pkcs5_pad($input, $size);
-        $td = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_ECB, '');
-        $iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-        mcrypt_generic_init($td, $key, $iv);
-        $data = mcrypt_generic($td, $input);
-        mcrypt_generic_deinit($td);
-        mcrypt_module_close($td);
-        $data = base64_encode($data);
+
+         $iv = "@#$%!@#$#$%!@#$%"; //"@@@@&&&&$$$$%%%%"; //you can change it.
+         $data = openssl_encrypt ( $input , "AES-128-CBC" , $key, 0, $iv );
+         return $data;
+     }
+
+    public static function decrypt($crypt, $sKey) {
+
+        $iv = "@#$%!@#$#$%!@#$%";
+        $data = openssl_decrypt ( $crypt , "AES-128-CBC" , $sKey, 0, $iv );
         return $data;
-    } 
 
-    private static function pkcs5_pad ($text, $blocksize) {
-        $pad = $blocksize - (strlen($text) % $blocksize);
-        return $text . str_repeat(chr($pad), $pad);
     }
-
-    public static function decrypt($sStr, $sKey) {
-
-        $decrypted= mcrypt_decrypt(
-            MCRYPT_RIJNDAEL_128,
-            $sKey,
-            base64_decode($sStr),
-            MCRYPT_MODE_ECB
-        );
-        $dec_s = strlen($decrypted);
-
-        $padding = ord($decrypted[$dec_s-1]);
-
-        $decrypted = substr($decrypted, 0, -$padding);
-        return $decrypted;
-    }
-
-    public static function encrypt1($input, $key)
-    {
-        //$key previously generated safely, ie: openssl_random_pseudo_bytes
-        $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
-        $iv = openssl_random_pseudo_bytes($ivlen);
-        $ciphertext_raw = openssl_encrypt($input, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
-        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
-        $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
-        return $ciphertext;
-    }
-
-    public static function decrypt1($sStr, $sKey){
-        $c = base64_decode($sStr);
-        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
-        $iv = substr($c, 0, $ivlen);
-        $hmac = substr($c, $ivlen, $sha2len=32);
-        $ciphertext_raw = substr($c, $ivlen+$sha2len);
-        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $sKey, $options=OPENSSL_RAW_DATA, $iv);
-        $calcmac = hash_hmac('sha256', $ciphertext_raw, $sKey, $as_binary=true);
-        if (hash_equals($hmac, $calcmac))//PHP 5.6+ timing attack safe comparison
-        {
-            return $original_plaintext;
-        }
-        else{
-            return "";
-        }
-    }
-
 }
 ?>
