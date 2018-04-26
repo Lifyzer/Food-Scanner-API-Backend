8a94f82ae0d1661903519e2c4e5701b21a096326
Pierre-Henry Soria <hi@ph7.me>
2018-04-26 22:25:06 +0100

    Use single quotes, when possible!

--- FoodScanApp/TableVars.php
+++ FoodScanApp/TableVars.php
@@ -1,9 +1,9 @@
 <?php
 
-define("TABLE_USER", "user");
-define("TABLE_PRODUCT", "product");
-define("TABLE_HISTORY", "history");
-define("TABLE_FAVOURITE", "favourite");
-define("TABLE_CATEGORY", "category");
-define("TABLE_ADMIN_CONFIG", "admin_config");
-define("TABLE_APP_TOKENS", "app_tokens");
+define('TABLE_USER', 'user');
+define('TABLE_PRODUCT', 'product');
+define('TABLE_HISTORY', 'history');
+define('TABLE_FAVOURITE', 'favourite');
+define('TABLE_CATEGORY', 'category');
+define('TABLE_ADMIN_CONFIG', 'admin_config');
+define('TABLE_APP_TOKENS', 'app_tokens');
