1582bb5d7e96a0e0ed5878392b60c23d8042f8b5
Pierre-Henry Soria <hi@ph7.me>
2018-05-07 13:18:30 +0100

    Remove useless variable assignation

--- Logger.php
+++ Logger.php
@@ -14,8 +14,6 @@ class Logger
 
     public function writetoFile($identifer, $content, $filename)
     {
-        $file = $filename;
-
         $logtime  = date('m/d/Y h:i:s a', time());
 
         // The new person to add to the file
@@ -23,6 +21,6 @@ class Logger
 
         // using the FILE_APPEND flag to append the content to the end of the file
         // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
-        file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+        file_put_contents($filename, $person, FILE_APPEND | LOCK_EX);
     }
 }
